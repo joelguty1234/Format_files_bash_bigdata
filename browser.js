0951600174430588b6341973eec44a843e2fd4f3
jmgutierreze <jmgutierreze@bch.bancodechile.cl>
2023-05-13 12:26:24 -0500

    MOD: Update return / after /download

--- browser.js
+++ browser.js
@@ -3,11 +3,11 @@ const multer = require('multer');
 const { processInputFile } = require('./correct');
 const fs = require('fs');
 const app = express();
+const onFinished = require('on-finished');
 
 let port = process.env.PORT || 3000;
 
 let inputFilePath , outputFilePath, separator_character = '', num_columns = 0, originalname = '';
-
 app.use(express.static('views'));
 
 const storage = multer.diskStorage({
@@ -23,15 +23,20 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('upload', { showDownload: false });
+  console.log("Inicio")
 });
 
 app.post('/upload', upload.single('file'), async (req, res) => {
+  try {
   originalname = req.file.originalname;
   separator_character = req.body.separator_character;
   num_columns = parseInt(req.body.num_columns);
-  res.send('File uploaded successfully!');
   await yourFunctionName();
-  //res.render('upload', { showDownload: true });
+  res.send('File uploaded successfully!');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred during file upload.');
+  }
 });
 
 async function yourFunctionName() {
@@ -48,23 +53,30 @@ async function yourFunctionName() {
   });
 }
 
-app.get('/download', (req, res) => {
-  const file = `${outputFilePath}`;
-  res.download(file); 
-  console.log("se acabo")
 
-  res.on('finish', function() {
-    console.log(`Download complete for file ${file}`);
-    fs.unlink(file, (err) => {
+app.get('/download', (req, res) => {
+  console.log("inicia descarga")
+  const file = `${outputFilePath}`;
+  res.download(file, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }else { 
+      console.log(`Download complete for file ${file}`);
+      fs.unlink(file, (err) => {
       if (err) {
         console.error(err);
         return;
       }
       console.log(`${file} was deleted`);
-    });
+    })};
   });
+
 });
+
 
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
 });
+
+
--- correct.js
+++ correct.js
@@ -1,55 +1,61 @@
 const fs = require('fs');
 const readline = require('readline');
 
-async function processInputFile(
-              inputFilePath, 
-              outputFilePath,
-              separator_character,
-              num_character) {
- 
-  const readStream = fs.createReadStream(inputFilePath);
-  const writeStream = fs.createWriteStream(outputFilePath);
+function processInputFile(
+  inputFilePath,
+  outputFilePath,
+  separator_character,
+  num_character
+) {
+  return new Promise((resolve, reject) => {
+    const readStream = fs.createReadStream(inputFilePath);
+    const writeStream = fs.createWriteStream(outputFilePath);
 
-  const rl = readline.createInterface({
-    input: readStream
-  });
+    const rl = readline.createInterface({
+      input: readStream
+    });
 
-  let currentLine = '';
-  let lineNumber = 0;
-  let header;
+    let currentLine = '';
+    let lineNumber = 0;
+    let header;
 
-  const processLine = (contenido_line) => {
-    lineNumber++;
-    if (lineNumber === 1) {
-      header = contenido_line;
-      writeStream.write(header + '\n');
-      return;
-    }
-    if (currentLine.split(separator_character).length - 1 < num_character) {
-      currentLine += contenido_line;
-    } else {
+    const processLine = (contenido_line) => {
+      lineNumber++;
+      if (lineNumber === 1) {
+        header = contenido_line;
+        writeStream.write(header + '\n');
+        return;
+      }
+      if (currentLine.split(separator_character).length - 1 < num_character) {
+        currentLine += contenido_line;
+      } else {
+        writeStream.write(currentLine + '\n');
+        currentLine = contenido_line;
+      }
+
+      // Pause and resume after every 100,000 lines
+      if (lineNumber % 100000 === 0) {
+        console.log("se colgo")
+        rl.pause();
+        setTimeout(() => {
+          rl.resume();
+        }, 100);
+      }
+    };
+
+    rl.on('line', processLine);
+
+    rl.on('close', () => {
       writeStream.write(currentLine + '\n');
-      currentLine = contenido_line;
-    }
-    
-    // Pause and resume after every 100,000 lines
-    if (lineNumber % 100000 === 0) {
-      console.log("se colgo")
-      rl.pause();
-      setTimeout(() => {
-        rl.resume();
-      }, 500);
-    }
-  };
+      writeStream.end(); // Close the write stream
+      console.log('Done!');
+      resolve(); // Resolve the promise when processing is complete
+    });
 
-  rl.on('line', processLine);
-  
-  rl.on('close', () => {
-    writeStream.write(currentLine + '\n');
-    console.log('Donex!');
-    writeStream.close();
+    rl.on('error', (error) => {
+      reject(error); // Reject the promise if an error occurs
+    });
   });
-
 }
 
 module.exports = {
